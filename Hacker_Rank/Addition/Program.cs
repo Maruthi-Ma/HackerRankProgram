89586a0b0524808d3a15afce971673b57eb3e2bd
Maruthi <maruthir@biztechnosys.com>
2020-02-04 05:44:50 -0600

    Added First Exercise

---
+++ Hacker_Rank/Addition/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addition
+{
+    class Program
+    {
+        static int solveMeFirst(int a, int b)
+        {
+            // Hint: Type return a+b; below  
+            return a + b;
+        }
+
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Enter value1");
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Value2");
+            int val2 = Convert.ToInt32(Console.ReadLine());
+            int sum = solveMeFirst(val1, val2);
+            Console.WriteLine(sum);
+        }
+    }
+}
