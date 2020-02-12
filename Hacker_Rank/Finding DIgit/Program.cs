8f8eb8c8d042eb04d129ac2e70cf437e36f55016
Maruthi <maruthir@biztechnosys.com>
2020-02-12 06:25:18 -0500

    beautiful days at the movies

---
+++ Hacker_Rank/Finding DIgit/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beautiful_Days_at_the_Movies
+{
+    class Program
+    {
+        static int beautifulDays(int i, int j, int k)
+        {
+            int reminder, result, count = 0;
+            for (int m = i; m <= j; m++)
+            {
+                var rev = 0;
+                var dummyDay = m;
+                while (dummyDay != 0)
+                {
+                    reminder = dummyDay % 10;
+                    rev *= 10 + reminder;
+                    dummyDay /= 10;
+                }
+                result = (Math.Abs(m - rev));
+                if (result % k == 0)
+                {
+                    count = count + 1;
+                }
+                Console.WriteLine(string.Format("The Value of Day is {0}, reversenumber is {1} ", m, rev));
+            }
+            return count;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            string[] ijk = Console.ReadLine().Split(' ');
+
+            int i = Convert.ToInt32(ijk[0]);
+
+            int j = Convert.ToInt32(ijk[1]);
+
+            int k = Convert.ToInt32(ijk[2]);
+
+            int result = beautifulDays(i, j, k);
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Hacker_Rank/Finding DIgit/reversenumber.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finding_DIgit
+{
+    class Reversenumber
+    {
+        static void numberReverse()
+        {
+            Console.WriteLine("Enter the number");
+            var inputnumber = Convert.ToInt32(Console.ReadLine());
+            var reminder = 0;
+            var rev = 0;
+            while (inputnumber != 0)
+            {
+                reminder = inputnumber % 10;
+                rev = rev * 10 + reminder;
+                inputnumber = inputnumber / 10;
+            }
+            Console.WriteLine(rev);
+        }
+
+    }
+
+
+    class Reversename
+    {
+        static void nameReverse()
+        {
+            Console.WriteLine("Enter the number");
+            var inputname = Console.ReadLine().ToLower();
+            StringBuilder sb = new StringBuilder();
+            for (int i = inputname.Length - 1; i >= 0; i++)
+            {
+                sb.Append(inputname[i]);
+            }
+            Console.WriteLine(sb);
+
+            //To check the Wheather palindrome or not
+            if (inputname == sb.ToString())
+            {
+                Console.WriteLine("Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("not palindrome");
+            }
+        }
+    }
+}
