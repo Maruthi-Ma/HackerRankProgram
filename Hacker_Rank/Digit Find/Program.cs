369dc41d38df76751700749c4e8a2a1de3b1b8c4
Maruthi <maruthir@biztechnosys.com>
2020-02-13 04:32:52 -0500

    find digit

---
+++ Hacker_Rank/Digit Find/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digit_Find
+{
+    class Program
+    {
+        static int findDigits(int n)
+        {
+            var data = n;
+            var reminder = 0;
+            var rever = 0;
+            var count = 0;
+            while (data > 0)
+            {
+                reminder = data % 10;
+                rever = rever * 10 + reminder;
+                data = data / 10;
+                if (reminder > 0 && n % reminder == 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                int result = findDigits(n);
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ Hacker_Rank/Drawing Book/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drawing_Book
+{
+    class Program
+    {
+        static int pageCount(int n, int p)
+        {
+
+            return 0;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int p = Convert.ToInt32(Console.ReadLine());
+
+            int result = pageCount(n, p);
+
+            Console.WriteLine(result);
+        }
+    }
+}
